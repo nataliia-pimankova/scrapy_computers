120769134d722a6e75273a130581e67b869dde93
Nataliia Pimankova <nataliia.pimankova@stxnext.pl>
2022-07-28 15:59:27 +0200

    items

--- computers/items.py
+++ computers/items.py
@@ -1,12 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class ComputersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    price = scrapy.Field()
+    processor_model = scrapy.Field()
+    graphics_card = scrapy.Field()
+    chipset = scrapy.Field()
+
